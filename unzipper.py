27ceda8dee0449964fd26a29b5648c7595c7bbf3
MrVergil <immortalyzy@gmail.com>
2023-03-02 21:51:11 +0100

    add time bar, add unzipsubfolder setting

--- unzipper.py
+++ unzipper.py
@@ -1,12 +1,15 @@
 """ MultiLevelUnzipper - unzip multiple levels of zip files at once """
+import argparse
 import json
 import os
 import shutil
 import subprocess
 import sys
+import time
 from datetime import datetime
-import argparse
 from pathlib import Path
+
+from tqdm import tqdm
 
 # for simplicity, use global variables to store write to file settings
 settings = {
@@ -16,6 +19,8 @@ settings = {
     "autodelete": False,
     "autodeleteexisting": False,
     "automoveup": False,
+    "unzipsubfolder": False,
+    "log_level": 3,
 }
 
 # the settings .json file will be stored in the users home directory
@@ -23,14 +28,15 @@ setting_file_name = ".MultiLevelUnzipperSettings.json"
 
 
 # log function, its settings are controlled by global variables
-def log_msg(message):
+def log_msg(message, log_level=3):
     """ log the message to the console and optionally to a file, automatically add time stamp """
-    time_message = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "-" + message
-    if settings["write_to_file"]:
-        with open(settings["log_file_name"], "a") as f:
-            f.write(time_message + "\n")
-    else:
-        print(time_message)
+    if log_level > settings["log_level"]:
+        time_message = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "-" + message
+        if settings["write_to_file"]:
+            with open(settings["log_file_name"], "a") as f:
+                f.write(time_message + "\n")
+        else:
+            print(time_message)
 
 
 # find the 7z.exe
@@ -49,6 +55,20 @@ def find7z():
             log_msg("7z.exe not found, please specify the path to 7z.exe")
             return None
     return path
+
+
+def dict_compare(d1, d2):
+    """ compare two dictionaries, return the added, removed, modified and same keys """
+    # this function is from https://stackoverflow.com/questions/4527942/comparing-two-dictionaries-and-checking-how-many-key-value-pairs-are-equal
+    # this function is use to check if the local setting file is from an older version
+    d1_keys = set(d1.keys())
+    d2_keys = set(d2.keys())
+    shared_keys = d1_keys.intersection(d2_keys)
+    added = d1_keys - d2_keys
+    removed = d2_keys - d1_keys
+    modified = {o: (d1[o], d2[o]) for o in shared_keys if d1[o] != d2[o]}
+    same = set(o for o in shared_keys if d1[o] == d2[o])
+    return added, removed, modified, same
 
 
 def read_settings():
@@ -79,16 +99,35 @@ def read_settings():
 
     # if the file exists, read the settings
     with open(settings_file, "r") as f:
-        settings = json.load(f)
+        # check if the settings are from an older version
+        settings_local = json.load(f)
+        added, removed, modified, same = dict_compare(settings, settings_local)
+        if len(added) > 0 or len(removed) > 0 or len(modified) > 0:
+            # setting file from an older version
+            new_settings = {**settings, **{k: v[1] for k, v in modified.items()}}
+            settings = new_settings
+            with open(settings_file, "w") as f:
+                json.dump(settings, f)
+            if len(added) > 0:
+                log_msg("new settings added: " + str(added))
 
         # check if the 7z.exe path is valid
         if not os.path.exists(settings["zip_excutible_path"]):
-            log_msg(
-                "7z.exe not found, please check the path to 7z.exe in "
-                + settings_file
-                + ", remember to use DOUBLE backslashes (\\\\) to escape the backslashes"
-            )
-            is_valid_setting = False
+            # try to find the 7z.exe
+            zip_path = find7z()
+            if zip_path is not None:
+                settings["zip_excutible_path"] = zip_path
+                # write the new settings to the file
+                with open(settings_file, "w") as f:
+                    json.dump(settings, f)
+                log_msg("7z path updated")
+            else:  # 7z.exe in setting file is not valid and cannot be found
+                log_msg(
+                    "7z.exe not found, please check the path to 7z.exe in "
+                    + settings_file
+                    + ", remember to use DOUBLE backslashes (\\\\) to escape the backslashes"
+                )
+                is_valid_setting = False
 
     # if the settings are not valid, return false, the program should not continue
     return is_valid_setting
@@ -106,6 +145,9 @@ def getPasswordList(dir):
         for line in f:
             passwordList.append(line.strip())
 
+    # print info message
+    log_msg(f"Found {len(passwordList):d} passwords")
+
     # TODO: add global password, if the global password is not empty, add it to the list
     # global password is stored in ~/.passwords.txt
     return passwordList
@@ -122,13 +164,13 @@ def unzipFileWith7z(
     # check if the file exists
     if not os.path.exists(file):
         log_msg(f"File {file} does not exist")
-        return
+        return False, lv
 
     # check if is a .lib, .dll or .exe file, if yes skip
     # TODO: maybe a temporary solution, need to find a way to unzip .lib files
     if file.endswith(".lib") or file.endswith(".dll") or file.endswith(".exe"):
         log_msg(f"File {file} is a .lib, .dll or .exe file, skipping...")
-        return
+        return False, lv
 
     # check if the output directory already exists
     if os.path.exists(f"{file}lv{lv:d}"):
@@ -137,7 +179,7 @@ def unzipFileWith7z(
             shutil.rmtree(f"{file}lv{lv:d}")
         else:
             log_msg(f"Output directory {file}lv{lv:d} already exists, skipping...")
-            return
+            return False, lv
 
     # unzip without password
     # verify file is a a os.PathLike
@@ -170,7 +212,7 @@ def unzipFileWith7z(
             log_msg(f'File "{file}" is not an archive')
         # remove the empty directory due to file is not an archive
         shutil.rmtree(f"{file}lv{lv:d}", ignore_errors=True)
-        return False
+        return False, lv
     if result.returncode == 0:
         log_msg(f"Archive {file} is not password protected, unzipped to {file}lv{lv:d}")
         right_pass_found = True
@@ -178,7 +220,7 @@ def unzipFileWith7z(
     elif password_protected is False:
         log_msg(f"Unknown error when unzipping {file}")
         log_msg(result.stderr.decode("utf-8", errors="replace"))
-        return False
+        return False, lv
 
     # unzip with password
     if password_protected:
@@ -204,7 +246,7 @@ def unzipFileWith7z(
     # no password found for the file
     if not right_pass_found:
         log_msg(f"Cannot find the correct password for {file}")
-        return False
+        return False, lv
 
     if has_archive:
         files = os.listdir(f"{file}lv{lv:d}")
@@ -253,7 +295,7 @@ def unzipFileWith7z(
             contains_folder = True
 
         if contains_exe and contains_folder:
-            log_msg(f"File {file} is a program, will not go to next level")
+            log_msg(f"Files unzipped from {file} is a program, will not go to next level")
             return False, lv
 
         # other files without .dll or .exe files will automatically stop at the next level
@@ -304,24 +346,98 @@ def main(target):
         for key, value in settings.items():
             print(f"\t{key} : {value}")
 
-    # if target is a directory
+    #################################### prepare for unzipping #####################################
+    # start timer
+    start_time = time.time()
+    total_files = 0
+    finished_files = 0
+    total_file_size = 0
+    finished_files_size = 0
+    successed = 0
+    failed = 0
+
+    # if target is a folder, get the total size of the files
     if os.path.isdir(target):
         for root, dirs, files in os.walk(target):
             for file in files:
-                unzipFileWith7z(
-                    os.path.join(root, file),
+                total_files += 1
+                total_file_size += os.path.getsize(os.path.join(root, file))
+    # convert to MB
+    total_file_size = total_file_size / 1024 / 1024
+
+    # display info
+    print(f"Total files: {total_files}, total size: {total_file_size:.2f} MB")
+
+    ##################################### start unzipping ##########################################
+    # if target is a directory
+    if os.path.isdir(target):
+        if settings["unzipsubfolder"]:
+            # unzip all files including files under subfolders
+            for root, dirs, files in os.walk(target):
+                for file in tqdm(files):
+                    success, lv = unzipFileWith7z(
+                        os.path.join(root, file),
+                        settings["zip_excutible_path"],
+                        passwords=passwords,
+                        autodelete=settings["autodelete"],
+                        autodeleteexisting=settings["autodeleteexisting"],
+                    )
+                    finished_files += 1
+                    finished_files_size += os.path.getsize(os.path.join(root, file)) / 1024 / 1024
+                    if success:
+                        log_msg(
+                            f"oooooooooooooooooooo Archive {file} has been unzipped to {file}lv{0:d}"
+                        )
+                        successed += 1
+                    else:
+                        log_msg(f"xxxxxxxxxxxxxxxxxxxx {file} cannot be unzipped.")
+                        failed += 1
+
+                    # estimate time remaining
+                    elapsed_time = time.time() - start_time
+                    speed = finished_files_size / elapsed_time
+                    left_time = (total_file_size - finished_files_size) / speed
+
+            if settings["automoveup"]:
+                dirs = os.listdir(target)
+                for dir_ in dirs:
+                    # if dir is a directory, it could just unzipped from a file
+                    #! it can also be a directory existing before the program runs
+                    if os.path.isdir(os.path.join(target, dir_)):
+                        move_files_up(os.path.join(target, dir_))
+        else:
+            # unzip all files in the target directory, but not including files under subfolders
+            list_of_files = os.listdir(target)
+            for file in tqdm(os.listdir(target)):
+                success, lv = unzipFileWith7z(
+                    os.path.join(target, file),
                     settings["zip_excutible_path"],
                     passwords=passwords,
                     autodelete=settings["autodelete"],
                     autodeleteexisting=settings["autodeleteexisting"],
                 )
-        if settings["automoveup"]:
-            dirs = os.listdir(target)
-            for dir_ in dirs:
-                # if dir is a directory, it could just unzipped from a file
-                #! it can also be a directory existing before the program runs
-                if os.path.isdir(os.path.join(target, dir_)):
-                    move_files_up(os.path.join(target, dir_))
+                finished_files += 1
+                finished_files_size += os.path.getsize(os.path.join(target, file)) / 1024 / 1024
+                if success:
+                    log_msg(
+                        f"oooooooooooooooooooo Archive {file} has been unzipped to {file}lv{0:d}"
+                    )
+                    successed += 1
+                else:
+                    log_msg(f"xxxxxxxxxxxxxxxxxxxx {file} cannot be unzipped.")
+                    failed += 1
+
+                # estimate time remaining
+                elapsed_time = time.time() - start_time
+                speed = finished_files_size / elapsed_time
+                left_time = (total_file_size - finished_files_size) / speed
+
+            if settings["automoveup"]:
+                for file in list_of_files:
+                    dir_ = os.path.join(target, file + "lv0")
+                    # move up only the list of files just unzipped
+                    if os.path.isdir(os.path.join(target, dir_)):
+                        move_files_up(os.path.join(target, dir_))
 
     # if target is a file
     if os.path.isfile(target):
